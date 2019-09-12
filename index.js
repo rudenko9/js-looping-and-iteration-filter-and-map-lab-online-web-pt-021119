35983386755d2ad303b0b38e73b3898cc703d920
Nataliya R <n.rudenko@gmail.com>
2019-09-12 03:09:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-  
+  for (const driver of drivers) {
+    if (driver.revenue === revenue){
+      console.log(driver.)
+    }
+  }
   
 }
